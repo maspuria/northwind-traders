5147a743531a6941545fd8cc8d7ca2b5a5a94a44
mariannaaspuria <aspuria.mp@gmail.com>
2025-12-01 14:02:34 -0600

    update pom.xl and app commit

---
+++ src/main/java/com/pluralsight/App.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+import java.sql.*;
+
+public class App {
+
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/northwind";
+        String username = "root";
+        String password = "yearup";
+
+        // 1. open a connection to the database
+        Connection connection = DriverManager.getConnection(url,username,password);
+
+        // create statement
+        // the statement is tied to the open connection
+        Statement statement = connection.createStatement();
+        // define your query
+        String query = "SELECT ProductName FROM products" ;
+
+        // 2. Execute your query
+        ResultSet results = statement.executeQuery(query);
+
+        // process the results
+        while (results.next()) {
+            String products = results.getString("ProductName");
+            System.out.println(products);
+        }
+
+        // 3. Close the connection
+        connection.close();
+
+    }
+}
