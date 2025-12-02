a5d5e94ac3c84f95218ca464ab32677a4a2b6c0c
mariannaaspuria <aspuria.mp@gmail.com>
2025-12-02 16:44:45 -0600

    update line 7 commit

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class App {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/northwind";
         String username = args[0];
         String password = args[1];
